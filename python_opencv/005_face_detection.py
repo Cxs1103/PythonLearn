3d4bfe75af5fdea2792ece65b1aa520749066848
test <test@163.com>
2021-11-07 12:49:25 +0800

    人脸检测

---
+++ python_opencv/005_face_detection.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@File  : 005_face_detection.py
+@Date  : 2021/11/7 12:06
+@Author: Cxs
+@Email: Cxs1103@163.com
+@Desc  : 
+'''
+# 导入cv模块
+import cv2.cv2 as cv
+
+# 人脸检测函数
+def face_detect_demo():
+    gary = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    face_detect = cv.CascadeClassifier(
+        'D:/Program Files/Python3.7/Lib/site-packages/cv2/data/haarcascade_frontalface_alt2.xml')
+    face = face_detect.detectMultiScale(gary,1.01,5,0,(100,100),(300,300))
+    for x, y, w, h in face:
+        cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=2)
+    cv.imshow('result', img)
+
+
+# 读取图片
+# img = cv.imread('./picture/20211107110136.png')
+img = cv.imread('./picture/20211107105519.jpg')
+
+# 调用检测函数
+face_detect_demo()
+
+# 等待
+while True:
+    if ord('q') == cv.waitKey(0):
+        break
+
+# 释放内存
+cv.destroyAllWindows()
